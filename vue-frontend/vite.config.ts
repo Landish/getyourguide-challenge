89717ee89e9ae784fd386f06ab0c358f94c04f66
Lado Lomidze <lado.lomidze@gmail.com>
2024-02-10 23:17:41 +0100

    wip

--- vue-frontend/vite.config.ts
+++ vue-frontend/vite.config.ts
@@ -7,8 +7,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: '0.0.0.0',
-    port: 5173
+    host: '0.0.0.0'
+    // port: 5173
   },
   resolve: {
     alias: {
