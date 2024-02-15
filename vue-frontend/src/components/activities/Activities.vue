982266e749462bbdf466faee65993b85ff2e4c1d
Lado Lomidze <lado.lomidze@gmail.com>
2024-02-15 15:42:08 +0100

    wip

--- vue-frontend/src/components/activities/Activities.vue
+++ vue-frontend/src/components/activities/Activities.vue
@@ -3,7 +3,7 @@ import { defineComponent } from 'vue'
 import type { Activity } from '@/types/types'
 import ActivitiesList from '@/components/activities/ActivitiesList.vue'
 import ActivitiesEmpty from '@/components/activities/ActivitiesEmpty.vue'
-import { getActivities } from '@/components/activities/services/activities-service'
+import { getActivities } from '@/services/activities-service'
 
 export default defineComponent({
   name: 'activities-component',
